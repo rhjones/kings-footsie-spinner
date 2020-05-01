909d5797d35f6501189cc6e846faac47c1a184bc
rhjones <rebekah.heacock@gmail.com>
2020-05-01 09:01:19 -0500

    Use class component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import React, {useState, useEffect} from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 const bodyParts = ['right hand', 'right foot', 'left hand', 'left foot'];
 const colors = ['red', 'green', 'yellow', 'blue'];
 
-const hexColors = {
-  red: '#ca303d',
-  green: '#a0a433',
-  yellow: '#eec634',
-  blue: '#436b6f',
-};
-
 const twisterOptions = bodyParts.reduce(
   (options, part) => [...options, ...colors.map(color => ({
-    hex: hexColors[color],
-    words: `${part} ${color}`,
+    color,
+    part,
     audio: new SpeechSynthesisUtterance(`${part} ${color}`)
   }))],
   []
@@ -22,37 +15,66 @@ const twisterOptions = bodyParts.reduce(
 
 const optionCount = twisterOptions.length;
 
-const makeSpin = () => twisterOptions[Math.floor(Math.random() * optionCount)];
+const getSpin = () => twisterOptions[Math.floor(Math.random() * optionCount)];
 
-function App() {
-  const [spin, setSpin] = useState(makeSpin());
-  const [isGoing, setIsGoing] = useState(false);
+class App extends Component {
+  state = {
+    spin: getSpin(),
+    isSpinning: false,
+  };
 
-  useEffect(() => {
-    let timer;
-    if (isGoing) {
-      timer = setInterval(() => {
-        const currentSpin = makeSpin();
-        setSpin(currentSpin);
-        window.speechSynthesis.speak(currentSpin.audio);
-      }, 1000);
+  setIsSpinning = () => {
+    this.setState(prevState => ({isSpinning: !prevState.isSpinning}), this.handleTimer);
+  };
+
+  executeSpin = () => {
+    const spin = getSpin();
+    this.setState({spin});
+    window.speechSynthesis.speak(spin.audio);
+  }
+
+  timer;
+
+  startSpinning = () => {
+    this.timer = setInterval(() => {
+      this.executeSpin();
+    }, 6000);
+  };
+
+  stopSpinning = () => {
+    clearInterval(this.timer);
+    this.setState({spin: getSpin()})
+  };
+
+  handleTimer = () => {
+    if (this.state.isSpinning) {
+      this.executeSpin();
+      this.startSpinning();
+    } else {
+      this.stopSpinning();
     }
-    return () => clearInterval(timer);
-  }, [isGoing]);
+  };
 
-  return (
-    <div className="App" >
-      <header className="App-header" style={{backgroundColor: spin.hex}}>
-        <p>{spin.words.toUpperCase()}</p>
-        <button
-          className="Btn"
-          onClick={() => setIsGoing(!isGoing)}
-        >
-          {isGoing ? 'stop' : 'start'}
-        </button>
-      </header>
-    </div>
-  );
+  componentWillUnmount = () => {
+    clearInterval(this.timer);
+  };
+
+  render() {
+    return (
+      <div className="App" >
+        <header className={`App-header ${this.state.spin.color}`}>
+          <p>{this.state.isSpinning ? `${this.state.spin.part} ${this.state.spin.color}`.toUpperCase() : 'CLICK TO START'}</p>
+          <button
+            className="Btn"
+            onClick={this.setIsSpinning}
+          >
+            {this.state.isSpinning ? 'stop' : 'start'}
+          </button>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
