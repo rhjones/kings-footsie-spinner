4ba90d4b167cde0006f5761dcf65e9c387b41683
rhjones <rebekah.heacock@gmail.com>
2020-05-01 12:41:46 -0500

    Updates

---
+++ precache-manifest.67cdbc627d5dc48c734290747dc322a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "437d81b81dbf48ba3363120a1d1d9907",
+    "url": "/kings-footsie-spinner/index.html"
+  },
+  {
+    "revision": "43fd64274e8f3630adc0",
+    "url": "/kings-footsie-spinner/static/css/main.ed396c5d.chunk.css"
+  },
+  {
+    "revision": "08a38882fea9423937ec",
+    "url": "/kings-footsie-spinner/static/js/2.c6e8c3dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kings-footsie-spinner/static/js/2.c6e8c3dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43fd64274e8f3630adc0",
+    "url": "/kings-footsie-spinner/static/js/main.3cdd5547.chunk.js"
+  },
+  {
+    "revision": "a2bdfd3862829ced0d0f",
+    "url": "/kings-footsie-spinner/static/js/runtime-main.6a839684.js"
+  }
+]);
