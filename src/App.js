2ebaf59fb77d76454b7d25aa807afb9ed878bb8a
rhjones <rebekah.heacock@gmail.com>
2020-05-01 08:30:52 -0500

    Basic version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+const bodyParts = ['right hand', 'right foot', 'left hand', 'left foot'];
+const colors = ['red', 'green', 'yellow', 'blue'];
+
+const hexColors = {
+  red: '#ca303d',
+  green: '#a0a433',
+  yellow: '#eec634',
+  blue: '#436b6f',
+};
+
+const twisterOptions = bodyParts.reduce(
+  (options, part) => [...options, ...colors.map(color => ({
+    hex: hexColors[color],
+    words: `${part} ${color}`,
+    audio: new SpeechSynthesisUtterance(`${part} ${color}`)
+  }))],
+  []
+);
+
+const optionCount = twisterOptions.length;
+
+const makeSpin = () => twisterOptions[Math.floor(Math.random() * optionCount)];
+
 function App() {
+  const [spin, setSpin] = useState(makeSpin());
+  const [isGoing, setIsGoing] = useState(false);
+
+  useEffect(() => {
+    let timer;
+    if (isGoing) {
+      timer = setInterval(() => {
+        const currentSpin = makeSpin();
+        setSpin(currentSpin);
+        window.speechSynthesis.speak(currentSpin.audio);
+      }, 1000);
+    }
+    return () => clearInterval(timer);
+  }, [isGoing]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App" >
+      <header className="App-header" style={{backgroundColor: spin.hex}}>
+        <p>{spin.words.toUpperCase()}</p>
+        <button
+          className="Btn"
+          onClick={() => setIsGoing(!isGoing)}
         >
-          Learn React
-        </a>
+          {isGoing ? 'stop' : 'start'}
+        </button>
       </header>
     </div>
   );
